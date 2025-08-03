020514b9379394799e84c4b9e0119ab2e4a384f5
Sidharth Surya <imsidharthsurya@gmail.com>
2025-08-03 20:03:14 +0530

    add file name input box

--- file-manager/src/FileManager.jsx
+++ file-manager/src/FileManager.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 const FileManager = ({ data, addNode, removeNode }) => {
   const [showFolder, setShowFolder] = useState({});
+  // storing for which id input should be visible
+  const [showInput, setShowInput] = useState(null);
+  const [nodeName, setNodeName] = useState("");
+  // storing whether we're creating file or folder
+  const [isFolder, setIsFolder] = useState(null);
   return (
     <div className="file-container">
-      {data.map((node, index) => {
+      {data.map((node) => {
         return (
           <div key={node.id}>
             <span
@@ -23,13 +28,31 @@ const FileManager = ({ data, addNode, removeNode }) => {
               <>
                 <span
                   className="content-name"
-                  onClick={() => addNode(node.id, "my file.js", false)}
+                  onClick={() => {
+                    setShowInput((prev) => {
+                      if (prev === node.id) {
+                        return null;
+                      }
+                      return node.id;
+                    });
+                    setNodeName("");
+                    setIsFolder(false);
+                  }}
                 >
                   📄
                 </span>
                 <span
                   className="content-name"
-                  onClick={() => addNode(node.id, "my folder", true)}
+                  onClick={() => {
+                    setShowInput((prev) => {
+                      if (prev === node.id) {
+                        return null;
+                      }
+                      return node.id;
+                    });
+                    setNodeName("");
+                    setIsFolder(true);
+                  }}
                 >
                   📁
                 </span>
@@ -39,6 +62,34 @@ const FileManager = ({ data, addNode, removeNode }) => {
             <span className="content-name" onClick={() => removeNode(node.id)}>
               🗑️
             </span>
+            {showInput === node.id && (
+              <div>
+                <input
+                  type="text"
+                  value={nodeName}
+                  onChange={(e) => setNodeName(e.target.value)}
+                />
+                <span
+                  className="content-name"
+                  onClick={() => {
+                    addNode(node.id, nodeName, isFolder);
+                    setShowInput(null);
+                    setNodeName("");
+                  }}
+                >
+                  ✅
+                </span>
+                <span
+                  className="content-name"
+                  onClick={() => {
+                    setShowInput(null);
+                    setNodeName("");
+                  }}
+                >
+                  ❌
+                </span>
+              </div>
+            )}
             {showFolder[node.name] && node.children && (
               <FileManager
                 data={node.children}
